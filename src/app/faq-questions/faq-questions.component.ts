b33a0c8986cb3b4f6ec9a1464345d407e4269a90
Abdul Irfan <unknown7k7k@gmail.com>
2024-12-05 15:26:40 +0530

    added custom icons

---
+++ src/app/faq-questions/faq-questions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-questions',
+  imports: [],
+  templateUrl: './faq-questions.component.html',
+  styleUrl: './faq-questions.component.css'
+})
+export class FaqQuestionsComponent {
+
+}
