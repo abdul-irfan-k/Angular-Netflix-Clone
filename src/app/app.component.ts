502c2c7ae10e6c2a0f3d56cb7a86da0c9bd09bd0
Abdul Irfan <unknown7k7k@gmail.com>
2024-12-05 17:35:22 +0530

    fixed angular-svg-icon issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from "./footer/footer.component";
-import { HeroContentBlockComponent } from "./hero-content-block/hero-content-block.component";
+import { FooterComponent } from './footer/footer.component';
+import { HeroContentBlockComponent } from './hero-content-block/hero-content-block.component';
+import { SvgIconComponent } from 'angular-svg-icon';
+// import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroContentBlockComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HeroContentBlockComponent,
+    SvgIconComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
