909ab9951de9127fafa1bab31dcc5aa5422cae84
Abdul Irfan <unknown7k7k@gmail.com>
2024-12-06 21:21:37 +0530

    refactor changed file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeroContentBlockComponent } from './hero-content-block/hero-content-block.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HeroContentBlockComponent } from './routes/home/hero-content-block/hero-content-block.component';
 import { SvgIconComponent } from 'angular-svg-icon';
-import { FaqQuestionsComponent } from './faq-questions/faq-questions.component';
+import { FaqQuestionsComponent } from './shared/components/faq-questions/faq-questions.component';
 // import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
